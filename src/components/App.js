a899f1ecada46611f09f14a868406696589d0600
Harris Kim <seura710@gmail.com>
2021-11-04 14:50:06 -0400

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import NavBar from "./NavBar.js";
+import Home from "./Home.js";
+import About from "./About.js";
 
 function App() {
   return (
@@ -11,3 +14,7 @@ function App() {
 }
 
 export default App;
+
+
+
+
